af86bf4828c90977fd3fbf55910c7169dbf4fa8f
sksayril <sksayril123@gmail.com>
2025-06-20 23:06:45 +0530

    cors problem

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ const adminCryptoRouter = require('./routes/admin.crypto.routes');
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 // File upload middleware
 app.use(fileUpload({
